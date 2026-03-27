b7cebb24e0cd2213a96a0fc40455bbd6893ce482
Shin Morisawa <git@tryh4rd.dev>
2026-03-27 21:34:25 +0900

    ok

--- pkgs/fonts.nix
+++ pkgs/fonts.nix
@@ -6,6 +6,8 @@ pkgs.stdenv.mkDerivation {
     src = ../assets/fonts;
     installPhase = ''
         mkdir -p $out/share/fonts/opentype
+        mkdir -p $out/share/fonts/truetype
         cp -R $src/**/*.otf $out/share/fonts/opentype/
+        cp -R $src/**/*.ttf $out/share/fonts/truetype/
     '';
 }
